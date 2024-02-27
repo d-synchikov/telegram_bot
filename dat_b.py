e8f7315dbf6940ac7c18aebc694afd774f66775f
d-synchikov <d_synchikov@mail.ru>
2024-02-28 01:38:46 +0300

    #

---
+++ dat_b.py
@@ -0,0 +1,54 @@
+
+
+quiz_data = [
+    {
+        'question': 'Что такое Python?',
+        'options': ['Язык программирования', 'Тип данных', 'Музыкальный инструмент', 'Змея на английском'],
+        'correct_option': 0
+    },
+    {
+        'question': 'Какой тип данных используется для хранения целых чисел?',
+        'options': ['int', 'float', 'str', 'natural'],
+        'correct_option': 0
+    },
+    {
+        'question': 'Какой тип данных используется для хранения строчных значений?',
+        'options': ['int', 'float', 'str', 'natural'],
+        'correct_option': 2
+    },
+    {
+        'question': 'В какие скобки заключен список?',
+        'options': ['[ ]', '( )', '{ }', '/ /'],
+        'correct_option': 0
+    },
+    {
+        'question': 'Каким ключевым словом начинается проверка условия?',
+        'options': ['of', 'if', 'def', 'in'],
+        'correct_option': 1
+    },
+    {
+        'question': 'Каким ключевым словом начинается запись функции?',
+        'options': ['for', 'def', 'if', 'echo'],
+        'correct_option': 1
+    },
+    {
+        'question': 'Набор переменных и функций, которые описывают какой-то объект?',
+        'options': ['Список', 'Группа', 'Матрица', 'Класс'],
+        'correct_option': 3
+    },
+    {
+        'question': 'Ключевое слово для создания класса?',
+        'options': ['def', 'class', 'create', 'run'],
+        'correct_option': 1
+    },
+    {
+        'question': 'Библиотека, встроенная в Python, предназначенная для асинхронного программирования?',
+        'options': ['asyncio', 'pandas', 'aiogram', 'math'],
+        'correct_option': 0
+    },
+    {
+        'question': 'Фреймворк для разработки Telegram ботов на языке программирования Python?',
+        'options': ['asyncio', 'pandas', 'aiogram', 'math'],
+        'correct_option': 2
+    },
+    ]
